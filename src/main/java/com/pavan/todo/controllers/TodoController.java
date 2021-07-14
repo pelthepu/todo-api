866f9ad86f6fc563bbacdef8dd336b6899b5e768
Pavan Elthepu <pavan.elthepu@gmail.com>
2021-07-14 17:00:43 +0530

    Enabled CORS

--- src/main/java/com/pavan/todo/controllers/TodoController.java
+++ src/main/java/com/pavan/todo/controllers/TodoController.java
@@ -9,6 +9,7 @@ import com.pavan.todo.services.TodoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/todos")
+@CrossOrigin
 public class TodoController {
 
     @Autowired

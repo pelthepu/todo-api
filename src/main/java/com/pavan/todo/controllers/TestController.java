0ec18f056b4bc0d04864fdf08cf5d598e6e26516
pelthepu <pavan.elthepu@gmail.com>
2024-02-02 14:42:52 +0530

    Added slow API for Prometheus Demo

---
+++ src/main/java/com/pavan/todo/controllers/TestController.java
@@ -0,0 +1,29 @@
+package com.pavan.todo.controllers;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+@CrossOrigin
+public class TestController {
+
+    @GetMapping("/slowApi")
+    public String timeConsumingAPI(@RequestParam(value = "delay", defaultValue = "0") Integer delay)
+            throws InterruptedException {
+        if (delay == 0) {
+            Random random = new Random();
+            delay = random.nextInt(10);
+        }
+
+        TimeUnit.SECONDS.sleep(delay);
+        return "Success";
+    }
+
+}
